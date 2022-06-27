bf37997d3d8aeea9066801962364a5156d02e498
Fabricio Trindade <fabrtrindade@gmail.com>
2022-06-27 19:19:50 -0300

    Refactoring the entire .css style file.

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -14,6 +14,9 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `
 
 
