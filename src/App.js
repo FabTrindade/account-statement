e040efa21cd1f4e3211d2ebed724c6538b40e7d1
FabTrindade <fabrtrindade@gmail.com>
2022-07-02 22:10:46 -0300

    Added theme change button.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GloalStyle";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas"
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toogleTema = () => {
+    setTema((tema) => !tema);
+  }
   return (
-    <>
+    <ThemeProvider theme = {tema? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toogleTema}>
+        <SwitcherTema tema={tema}/> 
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
